a1a01bbe307a20b5ad9630bfe1d9658af11b3a55
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-01-23 17:44:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {sculptureList} from './data';
+import './bootstrap.css';
+import {useState} from "react";
+import {HiArrowRightCircle,HiArrowLeftCircle,HiArrowRight,HiArrowLeft} from "react-icons/hi2";
+
 
 function App() {
+  const [index,setIndex]=useState(0);
+  const [showmore,setShowmore]=useState(false);
+  const taille=sculptureList.length;
+  function handleNextClick()
+  {
+    if(index + 1===sculptureList.length)
+    {
+      setIndex(0);
+    }else {
+      setIndex(index + 1);
+    }
+
+  }
+  function handlePrevClick()
+  {
+     if(index + 1 === 1)
+     {
+       setIndex(sculptureList.length-1);
+     }else
+     {
+       setIndex(index - 1);
+     }
+  }
+  function handleMoreClick()
+  {
+    setShowmore(!showmore);
+  }
+  let sculpture=sculptureList[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 col-md-5">
+      <button className="btn btn-outline-primary btn-sm m-1" onClick={handlePrevClick}>
+        <HiArrowLeftCircle style={{height:'30px'}}></HiArrowLeftCircle> Précedent
+      </button>
+      <button className="btn btn-outline-primary btn-sm" onClick={handleNextClick}>
+        Suivant <HiArrowRightCircle style={{height:'30px'}}></HiArrowRightCircle>
+      </button>
+      <div className="mt-3 border p-2 bg-dark mb-5 rounded">
+        <div className="bg-success p-3 text-white">
+          <h2>
+            <i>{sculpture.name}</i> par {sculpture.artist}
+          </h2>
+          <small>
+            ({index + 1} sur {sculptureList.length})
+          </small>
+          <br/>
+          <button className="btn btn-primary btn-sm mt-2" onClick={handleMoreClick}>
+            {showmore ? 'Masquer' : 'Afficher'} les détails
+          </button>
+        </div>
+        <div className="mt-3 text-white">
+          <p style={{fontWeight:'bold'}}>{showmore && sculpture.description}</p>
+        </div>
+        <br/>
+        <div className="mb-3">
+          <img
+              src={sculpture.url}
+              alt={sculpture.alt}
+              style={{width:'100%',height:'300px'}}
+              className="rounded"
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.jsx
@@ -0,0 +1,73 @@
+export const sculptureList = [{
+    name: 'Homenaje a la Neurocirugía',
+    artist: 'Marta Colvin Andrade',
+    description: 'Bien que Colvin soit principalement connue pour ses thèmes abstraits qui font allusion à des symboles préhispaniques, cette sculpture gigantesque, un hommage à la neurochirurgie, est l’une de ses pièces d’art public les plus reconnaissables.',
+    url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+    alt: 'Une statue en bronze de deux mains croisées tenant délicatement un cerveau humain du bout des doigts.'
+}, {
+    name: 'Floralis Genérica',
+    artist: 'Eduardo Catalano',
+    description: 'Cette immense fleur argentée (de 23 mètres de haut ou 75 pieds) est située à Buenos Aires. Elle est conçue pour bouger, en fermant ses pétales le soir ou lors de vents forts, et en les ouvrant le matin.',
+    url: 'https://i.imgur.com/ZF6s192m.jpg',
+    alt: 'Une sculpture de fleur métallique gigantesque avec des pétales réfléchissants, semblables à des miroirs, et des étamines solides.'
+}, {
+    name: 'Eternal Presence',
+    artist: 'John Woodrow Wilson',
+    description: 'Wilson était connu pour ses préoccupations d’égalité, de justice sociale, ainsi que les qualités essentielles et spirituelles de l’humanité. Ce bronze massif (de 2,13 mètres de hauteur ou 7 pieds) représente ce qu’il a décrit comme « une présence noire symbolique imprégnée d’un sens d’humanité universelle ».',
+    url: 'https://i.imgur.com/aTtVpES.jpg',
+    alt: 'La sculpture représentant une tête humaine semble être toujours présente et solennelle. Elle rayonne de calme et de sérénité.'
+}, {
+    name: 'Moai',
+    artist: 'Artiste inconnu·e',
+    description: 'Sur l’île de Pâques, il y a 1 000 moaïs, ou statues monumentales encore existantes, créées par les premiers habitants Rapa Nui, que certains croient représenter des ancêtres divinisés.',
+    url: 'https://i.imgur.com/RCwLEoQm.jpg',
+    alt: 'Trois bustes de pierre monumentaux avec des têtes démesurément grandes et des visages sombres.'
+}, {
+    name: 'Blue Nana',
+    artist: 'Niki de Saint Phalle',
+    description: 'Les Nanas sont des créatures triomphantes, symboles de féminité et de maternité. Initialement, Saint Phalle utilisait des tissus et des objets trouvés pour les Nanas, et plus tard, elle a introduit du polyester pour obtenir un effet plus vibrant.',
+    url: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    alt: 'Une grande sculpture en mosaïque d’une figure féminine fantaisiste dansant dans un costume coloré rayonnant de joie.'
+}, {
+    name: 'Ultimate Form',
+    artist: 'Barbara Hepworth',
+    description: 'Cette sculpture abstraite en bronze fait partie de la série « La Famille de l’Homme » située au parc de sculptures de Yorkshire. Hepworth a choisi de ne pas créer des représentations littérales du monde, mais a développé des formes abstraites inspirées des personnes et des paysages.',
+    url: 'https://i.imgur.com/2heNQDcm.jpg',
+    alt: 'Une sculpture haute composée de trois éléments empilés rappelant une figure humaine.'
+}, {
+    name: 'Cavaliere',
+    artist: 'Lamidi Olonade Fakeye',
+    description: "Descendant de quatre générations de sculpteurs sur bois, le travail de Fakeye mélangeait des thèmes yorubas traditionnels et contemporains.",
+    url: 'https://i.imgur.com/wIdGuZwm.png',
+    alt: 'Une sculpture en bois complexe représentant un guerrier au visage concentré sur un cheval orné de motifs.'
+}, {
+    name: 'Big Bellies',
+    artist: 'Alina Szapocznikow',
+    description: "Szapocznikow est connue pour ses sculptures du corps fragmenté en tant que métaphore de la fragilité et de l’impermanence de la jeunesse et de la beauté. Cette sculpture représente deux ventres très réalistes empilés l’un sur l’autre, chacun mesurant environ cinq pieds (1,5m) de hauteur.",
+    url: 'https://i.imgur.com/AlHTAdDm.jpg',
+    alt: 'La sculpture rappelle une cascade de plis, très différente des ventres dans les sculptures classiques.'
+}, {
+    name: 'Terracotta Army',
+    artist: 'Artiste inconnu·e',
+    description: 'L’Armée de terre cuite est une collection de sculptures en terre cuite représentant les armées de Qin Shi Huang, le premier empereur de Chine. L’armée était composée de plus de 8 000 soldats, 130 chars tirés par 520 chevaux et 150 chevaux montés par des cavaliers.',
+    url: 'https://i.imgur.com/HMFmH6m.jpg',
+    alt: '12 sculptures de guerriers solennels en terre cuite, chacun avec une expression faciale et une armure uniques.'
+}, {
+    name: 'Lunar Landscape',
+    artist: 'Louise Nevelson',
+    description: 'Nevelson était connue pour récupérer des objets dans les débris de la ville de New York, qu’elle assemblerait plus tard en constructions monumentales. Dans celle-ci, elle a utilisé des pièces disparates telles qu’un pied de lit, un bâton de jonglage et un fragment de siège, les clouant et les collant dans des boîtes qui reflètent l’influence de l’abstraction géométrique de l’espace et de la forme du cubisme.',
+    url: 'https://i.imgur.com/rN7hY6om.jpg',
+    alt: 'Une sculpture mate en noir où les éléments individuels sont initialement indiscernables.'
+}, {
+    name: 'Aureole',
+    artist: 'Ranjani Shettar',
+    description: 'Shettar fusionne le traditionnel et le moderne, le naturel et l’industriel. Son art se concentre sur la relation entre l’homme et la nature. Son travail a été décrit comme étant captivant à la fois de manière abstraite et figurative, défiant la gravité, et une « synthèse fine de matériaux improbables »."',
+    url: 'https://i.imgur.com/okTpbHhm.jpg',
+    alt: 'Une sculpture pâle en forme de fil de fer fixée au mur en béton et descendant jusqu’au sol. Elle semble légère.'
+}, {
+    name: 'Hippos',
+    artist: 'Zoo de Taipei',
+    description: 'Le zoo de Taipei a commandé des sculptures installées sur une place, représentant des hippopotames submergés en train de jouer.',
+    url: 'https://i.imgur.com/6o5Vuyu.jpg',
+    alt: 'Un groupe de sculptures d’hippopotames en bronze émergeant du trottoir en pavés comme s’ils nageaient.'
+}];
